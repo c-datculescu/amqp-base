0c1390894c5733e6b98844a2129ec1e3a5b2b251
Cristian Datculescu <cristian.datculescu@gmail.com>
2015-01-17 09:34:10 +0100

    Reorganised the examples folder

--- example/demo.php
+++ example/demo.php
@@ -1,12 +1,13 @@
 <?php
 /**
  * Simple example using s2 DI component for injecting the services
+ * Also showcases how to retrieve a queue/exchange to be used later
  */
 use \Symfony\Component\DependencyInjection\ContainerBuilder;
 use \Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use \Symfony\Component\Config\FileLocator;
 
-require_once "../../vendor/autoload.php";
+require_once "../vendor/autoload.php";
 
 $file = array(__DIR__);
 
@@ -19,5 +20,9 @@ $loader->load('services.yml');
 
 /** @var Builder\Amqp $res */
 $res = $container->get('amqpBuilder');
+
+// retrieve an exchange
 $res->exchange('shop.club.dev');
 
+// retrieve a queue
+$res->queue('test');
