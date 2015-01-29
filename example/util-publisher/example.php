d6cfa9ffa7f3b4375d44e5c89e247da39903fc22
Vitaliy Stepanyuk <vitaliy.stepanyuk@westwing.de>
2015-01-29 13:53:15 +0100

    Fixed publisher example

--- example/util-publisher/example.php
+++ example/util-publisher/example.php
@@ -1,32 +1,24 @@
 <?php
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-$configDir = array(__DIR__ . '/../config/');
-
-// initialize the base-non-di components:
-// the file locator to be used when searching for configuration
-$fileLocator = new Symfony\Component\Config\FileLocator($configDir);
-
-// the processor for validating the definitions imported from configuration
-$definitionProcessor = new Symfony\Component\Config\Definition\Processor;
-
-// create the config loader
-$configLoader = new \Amqp\Base\Config\YamlConfigLoader($fileLocator);
+$loader = new \Amqp\Base\Config\YamlConfigLoader();
+$config = $loader->load(__DIR__ . '/../util-listener/config/config.yml');
 
 // initialize the configuration factory
-$configFactory = new \Amqp\Base\Config\Processor($configLoader, $fileLocator, $definitionProcessor);
-
-// retrieve the configurations for publishers/consumers/amqp
-$configAmqp = $configFactory->getDefinition('config', new \Amqp\Base\Config\Amqp());
+$configFactory = new \Amqp\Base\Config\Processor($config);
 
 // set up the base-non-di builder
-$builder = new Amqp\Base\Builder\Amqp($configAmqp);
+$builder = new Amqp\Base\Builder\Amqp($configFactory);
 
-$configPublishers = $configFactory->getDefinition('config', new \Amqp\Util\Config\Publisher());
+$publisher = new Amqp\Util\Publisher\Simple(array(
+    'exchange' =>  'test',
+    'timeout' => '100000000'
+), $builder);
 
-$publisherBuilder = new \Amqp\Util\Builder\Publisher($configPublishers, $builder);
-$publisher = $publisherBuilder->publisher('publisher_test');
-
-$publisher->publish('test message', 'routed');
+for ($i = 0; $i < 10; $i++) {
+    $publisher->publish('test message ' . time(), '');
+    sleep(1);
+    echo $i . PHP_EOL;
+}
 
 
