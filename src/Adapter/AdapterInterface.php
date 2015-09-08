6979658e9fb68618cab812b2451835eb2c5fea7d
Cristian Datculescu <cristian.datculescu@westwing.de>
2015-09-08 17:21:18 +0200

    Added and commented interface for adapters

--- src/Adapter/AdapterInterface.php
+++ src/Adapter/AdapterInterface.php
@@ -3,9 +3,34 @@ namespace Amqp\Adapter;
 
 interface AdapterInterface
 {
-    public function publish($exchangeName, $message, $routingKey);
+    /**
+     * Publish a message onto an exchange
+     *
+     * @param string $exchangeName The name of the exchange to publish to
+     * @param string $message      The message to be published
+     * @param string $routingKey   The routing key to publish the message
+     *
+     * @return boolean
+     */
+    public function publish($exchangeName, $message, $routingKey = null);
 
-    public function listen($queue, callable $callable);
+    /**
+     * Listen for incoming messages in a blocking fashion.
+     *
+     * @param string   $queue    The queue on which we want to listen
+     * @param callable $callable The callable to apply upon receiving messages
+     * @param array    $options  The additional options to be applied when consuming messages [autoack => bool]
+     *
+     * @return void
+     */
+    public function listen($queue, callable $callable, array $options = array());
 
+    /**
+     * Returns a single message from the top of the queue
+     *
+     * @param string $queue The queue name
+     *
+     * @return mixed
+     */
     public function getMessage($queue);
 }
