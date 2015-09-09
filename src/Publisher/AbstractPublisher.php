aa0013930027e65ff43f73ef9413467a7c57bb62
Bela Bagameri <bela.bagameri@westwing.de>
2015-09-09 10:03:02 +0200

    Remove builder, add abstract publisher

---
+++ src/Publisher/AbstractPublisher.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Amqp\Publisher;
+
+use Amqp\Adapter\AdapterInterface;
+
+abstract class AbstractPublisher implements  PublisherInterface
+{
+    /**
+     * @var AdapterInterface
+     */
+    protected $adapter;
+
+    /**
+     * @param AdapterInterface $adapterInterface
+     */
+    public function __construct(AdapterInterface $adapterInterface)
+    {
+        $this->adapter = $adapterInterface;
+    }
+}
